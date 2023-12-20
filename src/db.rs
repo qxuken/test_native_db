174189c788f08c166dc922d0a75877653ecfc8c9
Nasonov V <qxuken@gmail.com>
2023-12-21 00:23:25 +0300

    Added parse cmd

--- src/db.rs
+++ src/db.rs
@@ -2,33 +2,44 @@ use crate::csv::CsvArtist;
 use image::{imageops::FilterType, io::Reader as ImageReader};
 use native_db::*;
 use native_model::{native_model, Model};
+use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::{
-    fs,
+    fmt::Display,
+    fs::{self, DirEntry},
     path::{Path, PathBuf},
 };
 use uuid::{NoContext, Timestamp, Uuid};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub enum ImgSize {
-    Full,
-    Cropped,
-    Thumbnail,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Img {
-    id: Uuid,
     path: PathBuf,
-    size: ImgSize,
     width: u32,
     height: u32,
+}
+
+impl Display for Img {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(w {:4}  h {:4})", self.width, self.height)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Paintings {
     id: Uuid,
-    imgs: Vec<Img>,
+    full: Img,
+    cropped: Img,
+    thumbnail: Img,
+}
+
+impl Display for Paintings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} ->  full {}  cropped {}  thumbnail {}",
+            self.id, self.full, self.cropped, self.thumbnail
+        )
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -46,6 +57,22 @@ pub struct Artist {
     paintings: Vec<Paintings>,
 }
 
+impl Display for Artist {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "{} -> {}  {} - {} [{}] [{}]",
+            self.id, self.name, self.born, self.died, self.nationality, self.genre
+        )?;
+        writeln!(f, "wikipedia: {}", self.wikipedia)?;
+        writeln!(f, "{}", self.bio)?;
+        for painting in &self.paintings {
+            writeln!(f, "  p {}", painting)?;
+        }
+        Ok(())
+    }
+}
+
 impl TryFrom<(CsvArtist, &Path, &Path)> for Artist {
     type Error = anyhow::Error;
 
@@ -53,7 +80,10 @@ impl TryFrom<(CsvArtist, &Path, &Path)> for Artist {
         (csv_artist, input_path, data_path): (CsvArtist, &Path, &Path),
     ) -> Result<Self, Self::Error> {
         let img_dest_path = data_path.join("img");
-        fs::create_dir_all(&img_dest_path)?;
+        if !img_dest_path.is_dir() {
+            fs::create_dir_all(&img_dest_path)
+                .map_err(|e| anyhow::anyhow!("Failed to create img directory: {}", e))?;
+        }
         let img_input_path = input_path.join("images").join(
             csv_artist
                 .name
@@ -62,29 +92,124 @@ impl TryFrom<(CsvArtist, &Path, &Path)> for Artist {
                 .collect::<Vec<String>>()
                 .join("_"),
         );
-        let resized_img_input_path = input_path.join("resized");
-        #[allow(clippy::never_loop)]
-        for entry in fs::read_dir(img_input_path)? {
-            let ts = Timestamp::from_unix(NoContext, 1497624119, 1234);
-            let img_id = Uuid::new_v7(ts);
-            let image = entry?;
-            let image_path = image.path();
-            let image_view = ImageReader::open(&image_path)?.decode()?;
-            let resized_img_path = resized_img_input_path.join(image.file_name());
-            println!("resized_img_path: {resized_img_path:?}");
-            println!("resized_img_path exists: {:?}", resized_img_path.is_file());
-            let dest_full_img_path = img_dest_path.join(format!("{}_full.jpg", &img_id));
-            let dest_resized_img_path = img_dest_path.join(format!("{img_id}_resized.jpg"));
-            let dest_thumnail_img_path = img_dest_path.join(format!("{img_id}_thumbnail.jpg"));
-            fs::copy(image_path, dest_full_img_path)?;
-            fs::copy(resized_img_path, dest_resized_img_path)?;
-            image_view
-                .resize(150, 150, FilterType::Lanczos3)
-                .save(dest_thumnail_img_path)?;
+        let paintings: Vec<Paintings> = fs::read_dir(img_input_path)?
+            .par_bridge()
+            .map(|entry| {
+                entry.map_err(|e| anyhow::anyhow!("Failed to read directory entry: {}", e))
+            })
+            .collect::<Result<Vec<DirEntry>, anyhow::Error>>()?
+            .par_iter()
+            .map(|image| {
+                let painting_id = Uuid::new_v7(Timestamp::now(NoContext));
+                let painting_dir = img_dest_path.join(painting_id.to_string());
+                if painting_dir.is_dir() {
+                    anyhow::bail!("Painting directory already exists")
+                }
+                if let Err(e) = fs::create_dir(&painting_dir) {
+                    println!("{csv_artist:?}");
+                    println!("{image:?}");
+                    println!("{painting_dir:?}");
+                    anyhow::bail!("Failed to create painting directory: {}", e)
+                }
 
-            anyhow::bail!("not implemented")
-        }
-        anyhow::bail!("not implemented")
+                let image_path = image.path();
+                let full_image = ImageReader::open(&image_path)
+                    .map_err(anyhow::Error::from)
+                    .and_then(|i| i.decode().map_err(anyhow::Error::from))
+                    .and_then(|image_view| {
+                        let img = Img {
+                            path: PathBuf::from(painting_id.to_string()).join("full.jpg"),
+                            width: image_view.width(),
+                            height: image_view.height(),
+                        };
+                        fs::copy(&image_path, img_dest_path.join(&img.path))
+                            .map_err(anyhow::Error::from)
+                            .map(|_| img)
+                    });
+                if let Err(e) = full_image {
+                    println!("{csv_artist:?}");
+                    println!("{image:?}");
+                    println!("{painting_dir:?}");
+                    anyhow::bail!("Failed to copy full image: {}", e)
+                }
+                let thumbnail_img = ImageReader::open(&image_path)
+                    .map_err(anyhow::Error::from)
+                    .and_then(|i| i.decode().map_err(anyhow::Error::from))
+                    .map(|image_view| image_view.resize(150, 150, FilterType::Lanczos3))
+                    .and_then(|image_view| {
+                        let img = Img {
+                            path: PathBuf::from(painting_id.to_string()).join("thumbnail.jpg"),
+                            width: image_view.width(),
+                            height: image_view.height(),
+                        };
+
+                        image_view
+                            .save(img_dest_path.join(&img.path))
+                            .map_err(anyhow::Error::from)
+                            .map(|_| img)
+                    });
+
+                if let Err(e) = thumbnail_img {
+                    println!("{csv_artist:?}");
+                    println!("{image:?}");
+                    println!("{painting_dir:?}");
+                    anyhow::bail!("Failed to copy thumbnail image: {}", e)
+                }
+                let cropped_img = ImageReader::open(&image_path)
+                    .map_err(anyhow::Error::from)
+                    .and_then(|i| i.decode().map_err(anyhow::Error::from))
+                    .map(|image_view| image_view.resize(600, 600, FilterType::Lanczos3))
+                    .and_then(|image_view| {
+                        let img = Img {
+                            path: PathBuf::from(painting_id.to_string()).join("cropped.jpg"),
+                            width: image_view.width(),
+                            height: image_view.height(),
+                        };
+
+                        image_view
+                            .save(img_dest_path.join(&img.path))
+                            .map_err(anyhow::Error::from)
+                            .map(|_| img)
+                    });
+                if let Err(e) = cropped_img {
+                    println!("{csv_artist:?}");
+                    println!("{image:?}");
+                    println!("{painting_dir:?}");
+                    anyhow::bail!("Failed to copy cropped image: {}", e)
+                }
+
+                Ok(Paintings {
+                    id: painting_id,
+                    full: full_image.unwrap(),
+                    cropped: cropped_img.unwrap(),
+                    thumbnail: thumbnail_img.unwrap(),
+                })
+            })
+            .collect::<Result<Vec<Paintings>, anyhow::Error>>()?;
+
+        let artist = {
+            let (born, died) = csv_artist
+                .years
+                .split_once(|c| c == '-' || c == '–')
+                .ok_or(anyhow::anyhow!(
+                    "Failed to parse years for artist {}",
+                    csv_artist.name
+                ))?;
+            let born = born.trim().to_string();
+            let died = died.trim().to_string();
+            Artist {
+                id: Uuid::new_v7(Timestamp::now(NoContext)),
+                name: csv_artist.name,
+                born,
+                died,
+                genre: csv_artist.genre,
+                nationality: csv_artist.nationality,
+                bio: csv_artist.bio,
+                wikipedia: csv_artist.wikipedia,
+                paintings,
+            }
+        };
+        Ok(artist)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 use db::Artist;
-use std::path::{Path, PathBuf};
+use rayon::prelude::*;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 mod csv;
 mod db;
@@ -21,14 +25,24 @@ impl Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Read {
-        #[arg(short, long, value_name = "FILE", default_value = "data.csv")]
-        csv: PathBuf,
+    All {
+        #[arg(long, value_name = "DIR", default_value = "./data")]
+        data_dir: PathBuf,
 
-        #[arg(value_name = "DIR", default_value = "./input")]
-        input: PathBuf,
+        #[arg(short, long, value_name = "FILE", default_value = "data.redb")]
+        db: PathBuf,
     },
-    Save {
+    FindById {
+        #[arg(long, value_name = "DIR", default_value = "./data")]
+        data_dir: PathBuf,
+
+        #[arg(short, long, value_name = "FILE", default_value = "data.redb")]
+        db: PathBuf,
+
+        #[arg()]
+        id: String,
+    },
+    ParseCsv {
         #[arg(long, value_name = "DIR", default_value = "./data")]
         data_dir: PathBuf,
 
@@ -50,36 +64,72 @@ impl Commands {
         csv::read_csv::<csv::CsvArtist>(&csv_path)
     }
 
-    fn save_cmd(input: &Path, csv: &Path, db: &Path, data_dir: &Path) -> Result<()> {
+    fn parse_csv(input: &Path, csv: &Path, db: &Path, data_dir: &Path) -> Result<()> {
         let db_path = data_dir.join(db);
+        if !data_dir.is_dir() {
+            fs::create_dir_all(data_dir)?;
+        }
         let db_builder = db::create_db_builder()?;
         let db = db_builder.create(db_path)?;
         let t_rw = db.rw_transaction()?;
-        let artists = Self::read_csv(input, csv)?;
+        let artists = Self::read_csv(input, csv)?
+            .into_par_iter()
+            .map(|artist| (artist, input, data_dir).try_into())
+            .collect::<Result<Vec<Artist>>>()?;
         for artist in artists.into_iter() {
-            let artist: Artist = (artist, input, data_dir).try_into()?;
             t_rw.insert(artist)?;
         }
         t_rw.commit()?;
         Ok(())
     }
 
+    fn all(data_dir: &Path, db: &Path) -> Result<Vec<Artist>> {
+        let db_path = data_dir.join(db);
+        if !data_dir.is_dir() {
+            fs::create_dir_all(data_dir)?;
+        }
+        let db_builder = db::create_db_builder()?;
+        let db = db_builder.create(db_path)?;
+        let t_r = db.r_transaction()?;
+        let artists = t_r.scan().primary()?.all().collect();
+
+        Ok(artists)
+    }
+    fn find_by_id(data_dir: &Path, db: &Path, id: &str) -> Result<Artist> {
+        let db_path = data_dir.join(db);
+        if !data_dir.is_dir() {
+            fs::create_dir_all(data_dir)?;
+        }
+        let db_builder = db::create_db_builder()?;
+        let db = db_builder.create(db_path)?;
+        let t_r = db.r_transaction()?;
+        t_r.get()
+            .primary(id)?
+            .ok_or(anyhow::anyhow!("Artist not found"))
+    }
+
     fn execute(&self) -> Result<()> {
         match &self {
-            Self::Read { input, csv } => {
-                let artists = Self::read_csv(input, csv)?;
+            Self::All { data_dir, db } => {
+                let artists = Self::all(data_dir, db)?;
 
                 for artist in artists.iter() {
-                    println!("{artist:?}");
+                    println!("{artist}");
                 }
                 Ok(())
             }
-            Self::Save {
+            Self::FindById { data_dir, db, id } => {
+                let artist = Self::find_by_id(data_dir, db, id)?;
+
+                println!("{artist}");
+                Ok(())
+            }
+            Self::ParseCsv {
                 data_dir,
                 db,
                 csv,
                 input,
-            } => Self::save_cmd(input, csv, db, data_dir),
+            } => Self::parse_csv(input, csv, db, data_dir),
         }
     }
 }
